b7b4c926a98e2afaedfb107419d7ddd705c6a22a
yeesheng <yeeshengtan1127@gmail.com>
2023-09-09 11:07:35 +0800

    cors, db connection string

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ global using Foods.Models;
 using Foods.Data;
 using Foods.Interfaces;
 using Foods.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IFoodService, FoodService>();
 builder.Services.AddScoped<IEnquiriesService, EnquiriesService>();
-builder.Services.AddDbContext<FopsContext>();
+builder.Services.AddDbContext<FopsContext>(
+    (options) => options.UseSqlServer("ConnectionStrings:DefaultConnection"));
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")  // Replace with your frontend's domain
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -29,5 +45,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowOrigins");
 
 app.Run();
